24b9081028741ef2c16c938a09069a4c67e55761
Przemyslaw Seneczko <contact@pjsen.eu>
2018-04-19 21:52:25 +0200

    Add on mouse over effect to adorner and do search do adorner click

--- BookmarksBase.Search/MainWindow.xaml.cs
+++ BookmarksBase.Search/MainWindow.xaml.cs
@@ -85,25 +85,9 @@ namespace BookmarksBase.Search
 
         void FindTxt_PreviewKeyDown(object sender, KeyEventArgs e)
         {
-            try
+            if (e.Key == Key.Return)
             {
-                if (e.Key == Key.Return)
-                {
-                    DataContext =
-                        _bookmarksEngine
-                            .DoSearch(_bookmarks, FindTxt.Text)
-                            .OrderByDescending(b => b.DateAdded)
-                            ;
-                }
-            }
-            catch (ArgumentException ae)
-            {
-                MessageBox.Show(
-                    "Cannot create regular expression from given pattern. " + ae.Message,
-                    "Error",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Error
-                );
+                DoSearch();
             }
         }
 
@@ -172,6 +156,27 @@ namespace BookmarksBase.Search
                 Clipboard.SetText(b.Url);
             }
         }
+
+        public void DoSearch()
+        {
+            try
+            {
+                DataContext =
+                    _bookmarksEngine
+                        .DoSearch(_bookmarks, FindTxt.Text)
+                        .OrderByDescending(b => b.DateAdded)
+                        ;
+            }
+            catch (ArgumentException ae)
+            {
+                MessageBox.Show(
+                    "Cannot create regular expression from given pattern. " + ae.Message,
+                    "Error",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error
+                );
+            }
+        }
     }
 
     #region Adorner
@@ -180,14 +185,68 @@ namespace BookmarksBase.Search
     {
         readonly VisualCollection _visualCollection;
         readonly Image _image;
+        readonly Border _border;
+        readonly SolidColorBrush _bckgrBrush;
+        readonly SolidColorBrush _borderBrush;
+
+        readonly SolidColorBrush _bckgrBrush2;
+        readonly SolidColorBrush _borderBrush2;
 
         public SearchIconAdorner(UIElement adornedElement) : base(adornedElement)
         {
+            _bckgrBrush = new SolidColorBrush(Colors.Gold);
+            _bckgrBrush.Opacity = 0.3d;
+            _borderBrush = new SolidColorBrush(Colors.DarkGoldenrod);
+
+            _bckgrBrush2 = new SolidColorBrush(Colors.White);
+            _bckgrBrush2.Opacity = 0;
+            _borderBrush2 = new SolidColorBrush(Colors.White);
+
             _visualCollection = new VisualCollection(this);
             _image = new Image();
+            _border = new Border();
+
+            _border.BorderBrush = _borderBrush2;
+            _border.BorderThickness = new Thickness(1);
+            _border.Background = _bckgrBrush2;
+
+            _border.Child = _image;
             _image.Source = new BitmapImage(new Uri("pack://application:,,,/BookmarksBase.Search;component/searchicon.png"));
-            _image.Stretch = Stretch.Uniform;
-            _visualCollection.Add(_image);
+            _image.Stretch = Stretch.Fill;
+
+            _visualCollection.Add(_border);
+        }
+
+        protected override void OnMouseEnter(MouseEventArgs e)
+        {
+            var b = VisualTreeHelper.GetChild(e.OriginalSource as DependencyObject, 0);
+            if (b is Border border)
+            {
+                _border.BorderBrush = _borderBrush;
+                _border.BorderThickness = new Thickness(1);
+                _border.Background = _bckgrBrush;
+            }
+        }
+
+        protected override void OnMouseLeave(MouseEventArgs e)
+        {
+            var b = VisualTreeHelper.GetChild(e.OriginalSource as DependencyObject, 0);
+            if (b is Border border)
+            {
+                _border.BorderBrush = _borderBrush2;
+                _border.BorderThickness = new Thickness(1);
+                _border.Background = _bckgrBrush2;
+            }
+        
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            var w = ((App)Application.Current).Windows[0] as MainWindow;
+            if (w != null)
+            {
+                w.DoSearch();
+            }
         }
 
         protected override Visual GetVisualChild(int index) => _visualCollection[index];
@@ -197,9 +256,9 @@ namespace BookmarksBase.Search
             double controlWidth = AdornedElement.RenderSize.Width;
             double controlHeight = AdornedElement.RenderSize.Height;
             double imgSize = controlHeight - 10;
-            _image.Width = imgSize;
-            _image.Height = imgSize;
-            _image.Arrange(new Rect(controlWidth - imgSize * 1.3, imgSize / 4 , imgSize, imgSize));
+            _border.Width = imgSize;
+            _border.Height = imgSize;
+            _border.Arrange(new Rect(controlWidth - imgSize * 1.3, imgSize / 4 , imgSize, imgSize));
             return finalSize;
         }
 
