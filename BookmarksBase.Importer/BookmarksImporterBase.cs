3c395408b6e6edfedf08203397c183fd1d341eba
Przemyslaw Seneczko <contact@pjsen.eu>
2022-09-14 08:45:01 +0200

    Fix importer bug of saving as erroneous when succeeded after 2nd or 3rd attempt

--- BookmarksBase.Importer/BookmarksImporterBase.cs
+++ BookmarksBase.Importer/BookmarksImporterBase.cs
@@ -133,6 +133,7 @@ abstract class BookmarksImporterBase : IDisposable
 
                 downloadedFileName = GenerateTempFileName();
                 File.WriteAllText(downloadedFileName, httpResponseString, Encoding.UTF8);
+                isSuccess = true;
 
                 break;
             }
